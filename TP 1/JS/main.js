ececba250252d105340d3c3a5491c63e6d6f7606
CesarFrancoCenturion <centurioncesarfranco@gmail.com>
2025-06-10 01:04:24 -0300

    Entrega N°1

---
+++ TP 1/JS/main.js
@@ -0,0 +1,129 @@
+//en esta verduleria solo se vende a kilos, menos no, solo kilos enteros (no medio kilo, ni tres cuartos, y tampoco un cuarto)
+
+
+const papa = 500
+const zapallo = 400
+const lechuga = 600
+const tomate = 1000
+
+
+let precioFinal = 0
+const verduras = []  
+
+
+//funcion para comprar las verduras y agregar verduras a la lista de compra
+function comprarVerdura (verdura, cantidad) {
+    let costoDeVerduraElegida = 0 
+    
+//para que sea reconocida la verdura que ingresa el usuario por el string  
+const verduraEnMinusculas = verdura.toLowerCase()
+
+    if (verduraEnMinusculas === "papa"){
+        costoDeVerduraElegida = papa*cantidad
+        
+        }else if ( verduraEnMinusculas === "zapallo"){
+            costoDeVerduraElegida = zapallo*cantidad
+
+        }else if (verduraEnMinusculas === "lechuga"){
+            costoDeVerduraElegida = lechuga*cantidad
+
+        }else if (verduraEnMinusculas === "tomate"){
+            costoDeVerduraElegida = tomate*cantidad
+        
+        }else {
+            alert("esa verdura no esta disponible o la escribiste mal, elija una de las verduras que se muestran en pantalla")
+        }        
+    verduras.push(verduraEnMinusculas)  
+        return costoDeVerduraElegida
+      
+}
+
+//funcion calcular descuento
+function calcularDescuento(total, porcentajeDescuento) {
+    if (porcentajeDescuento < 0 || porcentajeDescuento > 100) {
+        console.log("el porcentaje de descuento  debe estar entre 0 y 100")
+        return total
+    }
+    const montoDescuento = total * (porcentajeDescuento / 100)
+    const precioConDescuento = total - montoDescuento
+    console.log(`descuento aplicado ${porcentajeDescuento}%, el monto de descuento es: ${montoDescuento}, el precio con descuento final es: ${precioConDescuento}`)
+    return precioConDescuento
+
+}
+
+
+
+
+//funcion mostrar lista de compras
+function listaDeCompras (){
+   if(verduras.length === 0) {
+    return "tu lista de compras esta vacia"
+    
+    }else{
+        return "tus verduras son: " + verduras.join(" , ")
+    }  
+}
+
+//menu de opciones para el cliente
+let menu  
+
+while (menu !==5){
+       menu = parseInt(prompt("ingrese la opcion deseada: \n1) comprar las verduras \n2) ver la lista de sus verduras\n3) precio total a pagar: \n4) aplicar descuento: \n5) terminar compra "))
+switch(menu){
+    
+    case 1:
+        let verduraComprada = prompt("ingrese la verdura que quiere comprar:\n1) papa\n2) zapallo\n3) lechuga\n4) tomate")
+        let kilosComprados = parseInt(prompt("ingrese los KILOS que quiere comprar"))
+        
+        //realiza la compra 
+        const costoDeLaCompra = comprarVerdura(verduraComprada, kilosComprados)
+            if(costoDeLaCompra > 0 ){
+                precioFinal = precioFinal+costoDeLaCompra
+            alert(`la verdura elegida es: ${verduraComprada}\n la cantidad de kilos es: ${kilosComprados}\n el precio de la compra es: ${costoDeLaCompra} `)
+            }
+       
+        break 
+
+    case 2:
+        alert(listaDeCompras())
+        break
+
+    case 3: 
+        alert(`el precio final a pagar es: ${precioFinal}`)
+        break
+
+    case 4:
+        if (precioFinal === 0){
+                alert("para aplicar el descuento debe realizar una compra")
+                break
+        }
+        let porcentaje = parseInt(prompt("ingresar el descuento que quiere aplicar"))
+        let porcentajeDescuento = parseInt(porcentaje)
+
+        //verificacion de si el porcentaje es valido    
+        if (isNaN(porcentaje)){
+            alert("porcentaje de descuento invalido. ingresar un nueno porcentaje")
+            console.error(`error: ingreso un porcentaje de descuento no numerico ${porcentaje}`)
+            break
+        }
+        //aca se aplica el descuento
+        let precioConDescuento = calcularDescuento(precioFinal, porcentajeDescuento)
+        alert(`el descuento aplicado es de: ${porcentajeDescuento}`)
+        precioFinal =  precioConDescuento
+        console.log(`el precio final con descuento es de: ${precioFinal}`)
+        break
+
+        case 5:
+            alert("gracias por su compra :)")
+        break
+        
+    default:
+        alert("opcion no valida. Elegir una opcion del 1 al 5")
+        
+    }
+
+
+}
+
+ 
+
